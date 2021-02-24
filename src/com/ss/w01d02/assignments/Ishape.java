5cef1edd74ad33ecd2ae25e51d63f993bcc27e67
st-sandoval <michael.sandoval@smoothstack.com>
2021-02-24 22:04:20 +0000

    matrix done, on to shapes

--- src/com/ss/w01d02/assignments/Ishape.java
+++ src/com/ss/w01d02/assignments/Ishape.java
@@ -1,5 +1,8 @@
 package com.ss.w01d02.assignments;
 
 public interface Ishape {
+	
+	public Double pieSquared = Math.pow(Math.PI, 2);
 
+	public void calculateArea();
 }
--- src/com/ss/w01d02/assignments/MainClass.java
+++ src/com/ss/w01d02/assignments/MainClass.java
@@ -1,5 +1,34 @@
 package com.ss.w01d02.assignments;
 
 public class MainClass {
-
+	public static void main(String[] args) {
+		System.out.println(args);
+		
+		for(String arg:args) {
+			System.out.println(arg);
+		}
+		twoDee();
+		
+	}
+	
+	static public void twoDee() {
+		Integer max = 0;
+		Integer x = 0;
+		Integer[] position = new Integer[2];
+		
+		Integer[][] matrix = {{1,5}, {6, 1}, {3,2}};
+		for(Integer[] arr: matrix) {
+			
+			for(int y = 0; y < arr.length; y++) {
+				if(arr[y] > max) {
+					max = arr[y];
+					position[0] = x;
+					position[1] = y;
+				}
+			}
+			x++;
+		}
+		System.out.println("Max: " + max);
+		System.out.println("Position: [" + position[0] + "," + position[1] + "]");
+	}
 }
