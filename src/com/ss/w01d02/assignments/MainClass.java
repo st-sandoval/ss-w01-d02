e2686507b64736f08363a47d8a50cae3d44912b2
st-sandoval <michael.sandoval@smoothstack.com>
2021-02-25 00:05:26 +0000

    formatted main file test

--- src/com/ss/w01d02/assignments/MainClass.java
+++ src/com/ss/w01d02/assignments/MainClass.java
@@ -1,52 +1,54 @@
 package com.ss.w01d02.assignments;
+
 import java.util.Scanner;
 
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println(args);
 		Scanner scanny = new Scanner(System.in);
-		
-		for(String arg:args) {
+
+		for (String arg : args) {
 			Integer in = scanny.nextInt();
 			Double dou = scanny.nextDouble();
 			Long lo = scanny.nextLong();
 			Float fl = scanny.nextFloat();
 			System.out.println("arg: " + arg);
-			System.out.println("int: " +  int);
+			System.out.println("int: " + in);
 		}
-		
-		//matrix max and position
+		scanny.close();
+
+		// matrix max and position
 		System.out.println("Matrix max and position: ");
 		twoDee();
-		
-		//shape interface, classes, areas
+
+		// shape interface, classes, areas
 		System.out.println("Area of circle: ");
 		Circle circ = new Circle(4.5);
 		circ.calculateArea();
 		circ.display(circ.area);
-		
+
 		System.out.println("Area of square: ");
 		Square squa = new Square(4.5);
 		squa.calculateArea();
 		squa.display(squa.area);
-		
+
 		System.out.println("Area of triangle: ");
 		Triangle tri = new Triangle(3.1, 4.0);
 		tri.calculateArea();
 		tri.display(tri.area);
-		
+
 	}
-	
+
 	static public void twoDee() {
 		Integer max = 0;
 		Integer x = 0;
 		Integer[] position = new Integer[2];
-		
-		Integer[][] matrix = {{1,5}, {6, 1}, {3,2}};
-		for(Integer[] arr: matrix) {
-			
-			for(int y = 0; y < arr.length; y++) {
-				if(arr[y] > max) {
+
+		Integer[][] matrix = { { 1, 5 }, { 6, 1 }, { 3, 2 } };
+		for (Integer[] arr : matrix) {
+
+			for (int y = 0; y < arr.length; y++) {
+				if (arr[y] > max) {
 					max = arr[y];
 					position[0] = x;
 					position[1] = y;
